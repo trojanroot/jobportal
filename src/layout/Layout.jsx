0071dbef2018fa233b338ef7c0a3b4f78fd6bc13
trojanroot <thapaaryan926@gmail.com>
2026-08-16 14:12:46 +0545

    Routes

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import { Outlet } from "react-router-dom"
+function Layout() {
+  return (
+    <>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Layout
